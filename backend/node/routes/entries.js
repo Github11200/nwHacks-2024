545a6fe4f62ead6ea3f55738eaa188b8b97966c3
Github11200 <78944617+Github11200@users.noreply.github.com>
2024-01-20 18:19:09 -0800

    Tested all the functions for adding, removing, finding, and getting all the entries from the database.

--- backend/node/routes/entries.js
+++ backend/node/routes/entries.js
@@ -1,12 +1,20 @@
 const { express, cors } = require("../exports");
-const { addEntry, removeEntry } = require("../services/dbService");
+const {
+    addEntry,
+    removeEntry,
+    findEntry,
+    getAllEntries,
+} = require("../services/dbService");
 const router = express.Router();
 
 router.use(cors());
 router.get("/getEntries", async (req, res) => {
     await addEntry("date1", "title1", "goofy", 123);
     await addEntry("date2", "title2", "goofy2", 1266);
+    await addEntry("date3", "title3", "goof3", 1234444);
     await removeEntry(123);
+    console.log(await findEntry(1266));
+    console.log(await getAllEntries());
     res.send("return");
 });
 
--- backend/node/services/dbService.js
+++ backend/node/services/dbService.js
@@ -1,45 +1,68 @@
 const { MongoClient } = require("../exports").mongodb;
-const uri = "mongodb+srv://admin:cheesy@main.k98k84m.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+    "mongodb+srv://admin:cheesy@main.k98k84m.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
-// const databaseName = "entries";
-// const collectionName = "entriesCollection";
+const databaseName = "entries";
+const collectionName = "entriesCollection";
 
 const addEntry = async (date, title, text, id) => {
     try {
         await client.connect();
-        const db = await client.db("entries");
-        const collection = await db.collection("entriesCollection");
+        const db = await client.db(databaseName);
+        const collection = await db.collection(collectionName);
         await collection.insertOne({
             date: date,
             title: title,
             text: text,
-            id: id
+            id: id,
         });
         console.log("Entry was added to the database.");
     } catch {
         console.log("Could not add entry to the database.");
     }
-}
+};
 
-const removeEntry = async(id) => {
+const removeEntry = async (id) => {
     try {
         await client.connect();
-        const db = await client.db("entries");
-        const collection = await db.collection("entriesCollection");
+        const db = await client.db(databaseName);
+        const collection = await db.collection(collectionName);
 
         await collection.deleteOne({ id: id });
         console.log("Removed the entry from the database.");
     } catch {
-        console.log("Could not remove entry to the database");
+        console.log("Could not remove entry to the database.");
     }
-}
+};
+
+const findEntry = async (id) => {
+    try {
+        await client.connect();
+        const db = await client.db(databaseName);
+        const collection = await db.collection(collectionName);
+
+        return await collection.findOne({ id: id });
+    } catch {
+        console.log("Could not find the entry from the database.");
+    }
+};
 
 const getAllEntries = async () => {
-}
+    try {
+        await client.connect();
+        const db = await client.db(databaseName);
+        const collection = await db.collection(collectionName);
+
+        return await collection.find({}).toArray();
+    } catch {
+        console.log("Could not get all of the entries from the database.");
+    }
+};
 
 module.exports = {
     addEntry,
     removeEntry,
-    getAllEntries
-}
+    findEntry,
+    getAllEntries,
+};
