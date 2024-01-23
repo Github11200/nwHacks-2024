7527dbd7fc2106ab3eef4f23c845da2bada329b0
Github11200 <78944617+Github11200@users.noreply.github.com>
2024-01-22 18:55:34 -0800

    Updated code

--- backend/node/routes/entries.js
+++ backend/node/routes/entries.js
@@ -12,28 +12,28 @@ app.use(express.json());
 
 router.post("/addEntry", async (req, res) => {
     await addEntry(req.body.date, req.body.title, req.body.text)
-        .then(res.send(200))
+        .then(res.sendStatus(200))
         .catch((error) => {
-            res.send(error);
+            res.sendStatus(error);
         });
 });
 
 router.delete("/removeEntry", async (req, res) => {
     await removeEntry(123456)
         .then(res.send(200))
-        .catch((error) => res.send(error));
+        .catch((error) => res.sendStatus(error));
 });
 
 router.get("/findEntry", async (req, res) => {
     await findEntry(123456)
         .then((entry) => res.send(entry))
-        .catch((error) => res.send(error));
+        .catch((error) => res.sendStatus(error));
 });
 
 router.get("/getAllEntries", async (req, res) => {
     await getAllEntries()
         .then((entries) => res.send(entries))
-        .catch((error) => res.send(error));
+        .catch((error) => res.sendStatus(error));
 });
 
 module.exports = router;
